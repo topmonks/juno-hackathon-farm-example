8556be8a2b23c68565e15d32448ddbf7a2257ad1
Miloslav Nenadal <nenadalm@gmail.com>
2023-07-28 08:56:22 +0200

    Show helpful errors when deployment fails

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ shopt -s inherit_errexit
 
 ADMIN="${1:-juno1zk4c4aamef42cgjexlmksypac8j5xw7n3s4wrd}"
 KOMPLE_MINT_ADDR="${KOMPLE_MINT_ADDR:-juno17rth4jstxs7cmrusvyluwlnt34l80cxaz7nufpjfntts00pk79asjxelgs}"
+NOIS_PROXY="${NOIS_PROXY:-juno17xgzprnr0j4cs72t7xfsr2lsmw8guwj640tr8e96nld524f0gmhq0hewqc}"
 
 JUNOFARMS_PATH='../junofarms'
 
@@ -12,6 +13,21 @@ function update_contract_address {
   local new_address="${1}"
 
   sed -i "s/\(VITE_CONTRACT_ADDRESS=\).*/\1${new_address}/g" "${JUNOFARMS_PATH}/package/ui/.env"
+}
+
+function assert_response_success {
+  local response="${1}"
+  local msg="${2}"
+
+  local code
+  code="$(echo "${response}" | jq -r '.code')"
+  if [[ "${code}" -ne 0 ]]; then
+      echo "[ERROR] ${msg}:" >&2
+      echo "${response}" >&2
+      return 1
+  fi
+
+  return 0
 }
 
 function compile {
@@ -24,14 +40,8 @@ function compile {
 function upload_code {
   local response
   response="$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 tx wasm store artifacts/juno_farm_hackathon_template.wasm --from "${ADMIN}" --gas-prices 0.075ujunox --gas auto --gas-adjustment 1.1 -o json -y)"
+  assert_response_success "${response}" "Uploading code failed"
 
-  local code
-  code="$(echo "${response}" | jq -r '.code')"
-  if [[ "${code}" -ne 0 ]]; then
-      echo "[ERROR] Uploading code failed:" >&2
-      echo "${response}" >&2
-      return 1
-  fi
   
   echo "[DEBUG] Code uploaded: ${response}" >&2
 
@@ -39,7 +49,10 @@ function upload_code {
   tx_hash="$(echo "${response}" | jq -r '.txhash')"
 
   sleep 10
-  junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json | jq '.logs[0].events[] | select(.type=="store_code") | .attributes[] | select(.key=="code_id") | .value' -r
+  response="$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json)" 
+  assert_response_success "${response}" "Uploading code failed"
+  
+  echo "${response}" | jq '.logs[0].events[] | select(.type=="store_code") | .attributes[] | select(.key=="code_id") | .value' -r
 }
 
 function upload_noise_code {
@@ -55,51 +68,57 @@ function upload_noise_code {
     --gas-prices 0.025ujunox \
     --broadcast-mode=async \
     --node=https://juno-testnet-rpc.polkachu.com:443 -o json -y)"
+  assert_response_success "${response}" "Uploading noise code failed"
 
-  local code
-  code="$(echo "${response}" | jq -r '.code')"
-  if [[ "${code}" -ne 0 ]]; then
-      echo "[ERROR] Uploading noise code failed:" >&2
-      echo "${response}" >&2
-      return 1
-  fi
-  
   echo "[DEBUG] Noise code uploaded: ${response}" >&2
 
   local tx_hash
   tx_hash="$(echo "${response}" | jq -r '.txhash')"
 
   sleep 10
-  junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json | jq '.logs[0].events[] | select(.type=="store_code") | .attributes[] | select(.key=="code_id") | .value' -r
+  response="$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json)"
+  assert_response_success "${response}" "Uploading noise code failed"
+  
+  echo "${response}" | jq '.logs[0].events[] | select(.type=="store_code") | .attributes[] | select(.key=="code_id") | .value' -r
 }
 
 function instantiate {
   local code_id="${1}"
 
-  local instantiate_msg
-  instantiate_msg=$(cat <<-END
+  local msg
+  msg=$(cat <<END
       {
-        "admin": "%s",
-        "komple_mint_addr": "%s"
+        "admin": "${ADMIN}",
+        "komple_mint_addr": "${KOMPLE_MINT_ADDR}"
       }
 END
   )
 
-  local msg
-  msg="$(printf "${instantiate_msg}" "$ADMIN" "${KOMPLE_MINT_ADDR}")"
+  echo "[DEBUG] Instantiating with: ${msg}" >&2
+
+  local response
+  response=$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 tx wasm instantiate "${code_id}" "${msg}" --from "${ADMIN}" --admin "${ADMIN}" --gas-prices 0.075ujunox --gas auto --gas-adjustment 1.1 --label " " -o json -y)
+  assert_response_success "${response}" "Instantiating failed"
 
   local tx_hash
-  tx_hash=$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 tx wasm instantiate "${code_id}" "${msg}" --from "${ADMIN}" --admin "${ADMIN}" --gas-prices 0.075ujunox --gas auto --gas-adjustment 1.1 --label " " -o json -y | jq '.txhash' -r)
-  sleep 10
+  tx_hash="$(echo "${response}" | jq -r '.txhash')"
 
-  junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json | jq 'last(.logs[0].events[] | .attributes[] | select(.key=="_contract_address") | .value)' -r
+  sleep 10
+  response="$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json)"
+  assert_response_success "${response}" "Instantiating failed"
+
+  echo "${response}" | jq 'last(.logs[0].events[] | .attributes[] | select(.key=="_contract_address") | .value)' -r
 }
 
 function migrate {
     local code_id="${1}"
     local contract_addr="${2}"
 
-    tx_hash=$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 --from "${ADMIN}" tx wasm migrate "${contract_addr}" "${code_id}" '{}' --gas-prices 0.075ujunox --gas auto --gas-adjustment 1.2 -o json -y | jq '.txhash' -r )
+    local response
+    response="$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 --from "${ADMIN}" tx wasm migrate "${contract_addr}" "${code_id}" '{}' --gas-prices 0.075ujunox --gas auto --gas-adjustment 1.2 -o json -y)"
+    assert_response_success "${response}" "Migration failed"
+
+    tx_hash=$(echo "${response}" | jq '.txhash' -r )
     echo "Migration TX hash: ${tx_hash}"
 }
 
@@ -139,14 +158,7 @@ EOF
     --gas-prices 0.025ujunox \
     --broadcast-mode=sync \
     --node=https://juno-testnet-rpc.polkachu.com:443 -o json -y)"
-
-  local code
-  code="$(echo "${response}" | jq -r '.code')"
-  if [[ "${code}" -ne 0 ]]; then
-      echo "[ERROR] Instantiating noise failed:" >&2
-      echo "${response}" >&2
-      return 1
-  fi
+  assert_response_success "${response}" "Instantiating noise failed"
   
   echo "[DEBUG] Noise code instantiated: ${response}" >&2
 
@@ -154,7 +166,10 @@ EOF
   tx_hash="$(echo "${response}" | jq -r '.txhash')"
 
   sleep 10
-  junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json | jq | jq '.logs[0].events[]|select(.type=="wasm").attributes[]|select(.key=="_contract_address").value' -r
+  response="$(junod --chain-id uni-6 --node https://juno-testnet-rpc.polkachu.com:443 query tx "${tx_hash}" -o json)"
+  assert_response_success "${response}" "Instantiating noise failed"
+
+  echo "${response}" | jq '.logs[0].events[]|select(.type=="wasm").attributes[]|select(.key=="_contract_address").value' -r
 }
 
 function deploy_new {
